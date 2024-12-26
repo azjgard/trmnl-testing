76d2d1bedac987e29aec248c4d402205df96cd65
Jordin Gardner <jordin@dittowords.com>
2024-12-26 14:07:52 -0700

    update sample data

--- index.ts
+++ index.ts
@@ -23,6 +23,18 @@ const MOCK_HABITS: Habit[] = [
       "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ficons%2Frunning&psig=AOvVaw2krMed8P7SC5ocWLFxutAe&ust=1735332366084000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCv7f2mxooDFQAAAAAdAAAAABAE",
     days_this_week: [true, false, true, false, true, false, true],
   },
+  {
+    name: "Lifting Weights",
+    icon_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ficons%2Frunning&psig=AOvVaw2krMed8P7SC5ocWLFxutAe&ust=1735332366084000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCv7f2mxooDFQAAAAAdAAAAABAE",
+    days_this_week: [false, false, false, false, false, false, true],
+  },
+  {
+    name: "Training BJJ",
+    icon_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ficons%2Frunning&psig=AOvVaw2krMed8P7SC5ocWLFxutAe&ust=1735332366084000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCv7f2mxooDFQAAAAAdAAAAABAE",
+    days_this_week: [true, true, true, true, false, false, false],
+  },
 ];
 
 app.get("/", (_req, res) => {
